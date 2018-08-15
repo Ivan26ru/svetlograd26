6a84d8c3356822238832272e5dde5c6d4519ee17
Ivan26ru <it-1ivandex.ru>
2018-08-16 01:54:01 +0300

    убрал лишнюю верстку

--- themes/svetlograd26/moduls/tax/single_organizacii.php
+++ themes/svetlograd26/moduls/tax/single_organizacii.php
@@ -70,19 +70,7 @@ if( $images ): ?>
     </li>
   </ul>
 </div>
-            	<hr>
-              <ul class="info_list">
-                <li><span class="info_name">name01</span><span class="info_value">value001</span></li>
-                <li><span class="info_name">name02</span><span class="info_value">value002</span></li>
-                <li><span class="info_name">name03</span><span class="info_value">value003</span></li>
-                <li><span class="info_name">name04</span><span class="info_value">value004</span></li>
-                <li><span class="info_name">name05</span><span class="info_value">value005</span></li>
-                <li><span class="info_name">name06</span><span class="info_value">value006</span></li>
-                <li><span class="info_name">name07</span><span class="info_value">value007</span></li>
-                <li><span class="info_name">name08</span><span class="info_value">value008</span></li>
-                <li><span class="info_name">name09</span><span class="info_value">value009</span></li>
-                <li><span class="info_name">name10</span><span class="info_value">value010</span></li>
-              </ul>
+
               <div class="rejim">
               	<?php if( have_rows('rejim') ): ?>
 
@@ -128,40 +116,6 @@ echo '</li>';
 <?php endif; ?>
               </div>
             </div>
-              <div class="row">
-                <div class="col-6">
-                  <p class="the_flat_capture">Квартира</p>
-                  <p class="the_flat_description">Количество комнат <span class="gray_line">_________________</span></p>
-                  <p class="the_flat_description">Этаж <span class="gray_line">_________________________________</span></p>
-                  <p class="the_flat_description">Общая площадь <span class="gray_line">____________________</span></p>
-                  <p class="the_flat_description">Площадь кухни <span class="gray_line">_____________________</span></p>
-                  <p class="the_flat_description">Жилая площадь <span class="gray_line">____________________</span></p>
-                  <p class="the_flat_description">Санузел <span class="gray_line">_____________________________</span></p>
-                  <p class="the_flat_description">Балкон <span class="gray_line">______________________________</span></p>
-                  <p class="the_flat_capture">Дом</p>
-                  <p class="the_flat_description">Тип здания <span class="gray_line">_________________________</span></p>
-                  <p class="the_flat_description">Серия здания <span class="gray_line">_______________________</span></p>
-                  <p class="the_flat_description">Год постройки <span class="gray_line">______________________</span></p>
-                  <p class="the_flat_description">Лифт <span class="gray_line">_________________________________</span></p>
-                  <p class="the_flat_description">Жилой комплекс <span class="gray_line">____________________</span></p>
-                </div>
-                <div class="col-6">
-                  <p class="the_flat_capture">&nbsp;</p>
-                  <p class="the_flat_description">3</p>
-                  <p class="the_flat_description">5 из 23</p>
-                  <p class="the_flat_description">63,4 м<sup>2</sup></p>
-                  <p class="the_flat_description">11 м<sup>2</sup></p>
-                  <p class="the_flat_description">48 м<sup>2</sup></p>
-                  <p class="the_flat_description">раздельный</p>
-                  <p class="the_flat_description">да</p>
-                  <p class="the_flat_capture">&nbsp;</p>
-                  <p class="the_flat_description">монолитное</p>
-                  <p class="the_flat_description">индивидуальный проект</p>
-                  <p class="the_flat_description">2013</p>
-                  <p class="the_flat_description">да</p>
-                  <p class="the_flat_description">Андреевский</p>
-                </div>
-              </div>
             </div>
           </div>
 
