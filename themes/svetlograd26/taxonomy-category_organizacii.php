113cedf3a9193df48bf21c213aedd1537e590fcf
Ivan26ru <it-1ivan@yandex.ru>
2018-08-20 12:15:01 +0300

    закомментировал блок выбора города

--- themes/svetlograd26/taxonomy-category_organizacii.php
+++ themes/svetlograd26/taxonomy-category_organizacii.php
@@ -3,6 +3,6 @@ get_header(); // подключаем header.php
 
 include (TEMPLATEPATH . '/moduls/tax/category_organizacii.php');
 include (TEMPLATEPATH . '/moduls/index/top-organizations.php');
-include (TEMPLATEPATH . '/moduls/index/ads-gorod.php');
+// include (TEMPLATEPATH . '/moduls/index/ads-gorod.php');
 get_footer(); // подключаем footer.php
 ?>
