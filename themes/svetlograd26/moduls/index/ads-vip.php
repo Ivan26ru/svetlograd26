16c906d13a134688414cd661b6c87df4d6d0c53b
Ivan26ru <it-1ivan@yandex.ru>
2018-09-03 09:21:21 +0300

    Вывел отдельный модуль VIP объявлений

---
+++ themes/svetlograd26/moduls/index/ads-vip.php
@@ -0,0 +1,25 @@
+<section class="vip_ads_flats">
+  <h2 class="vip_ads_caption">VIP ОБЪЯВЛЕНИЯ</h2>
+  <div class="vip_flats">
+    <div class="vip_slide_1">
+      <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide2.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide3.jpg" alt="">
+    </div>
+    <div class="vip_slide_2">
+      <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide3.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide1.jpg" alt="">
+      <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide2.jpg" alt="">
+    </div>
+  </div>
+  <!-- слайдер для адаптива -->
+  <div class="vip_flats2">
+    <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide1.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide2.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide3.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide1.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide2.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri();  ?>/images/flats_slide3.jpg" alt="">
+  </div>
+</section>
+<!-- /.vip_ads -->
