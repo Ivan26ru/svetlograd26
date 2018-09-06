32deead5d981ea082c68ee23308697175d8b21b6
Ivan26ru <it-1ivan@yandex.ru>
2018-09-06 09:08:45 +0300

    Вывод контента страницы в отдельный файл

---
+++ themes/svetlograd26/moduls/page/page_content.php
@@ -0,0 +1,8 @@
+<div class="sections">
+	<h2 class="caption"><?php the_title(); ?></h2>
+	<!-- <div class="container"> -->
+		<!-- <div class="row"> -->
+			<?php the_content(); ?>
+		<!-- </div> -->
+	<!-- </div> -->
+</div>
--- themes/svetlograd26/page.php
+++ themes/svetlograd26/page.php
@@ -2,17 +2,8 @@
 get_header(); // подключаем header.php
 // include (TEMPLATEPATH . '/moduls/index/ads-cat.php');
 // include (TEMPLATEPATH . '/moduls/carausel/vip-ads.php');
-?>
-<div class="sections">
-	<h2 class="caption"><?php the_title(); ?></h2>
-	<!-- <div class="container"> -->
-		<!-- <div class="row"> -->
-			<?php the_content(); ?>
-		<!-- </div> -->
-	<!-- </div> -->
-</div>
-<?php
 // include (TEMPLATEPATH . '/moduls/index/top-organizations.php');
+include (TEMPLATEPATH . '/moduls/page/page_content.php');
 include (TEMPLATEPATH . '/moduls/carausel/partners.php');
 // include (TEMPLATEPATH . '/moduls/index/vibor-nas-100.php');
 // include (TEMPLATEPATH . '/moduls/index/ads-gorod.php');
