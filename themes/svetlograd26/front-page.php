8cb8c598651d8d33b7c0555b5a0acbbb9032ae99
Ivan26ru <it-1ivan@yandex.ru>
2018-09-06 08:50:14 +0300

    шаблон главной страницы

---
+++ themes/svetlograd26/front-page.php
@@ -0,0 +1,10 @@
+<?php
+get_header(); // подключаем header.php
+// include (TEMPLATEPATH . '/moduls/index/ads-cat.php');
+// include (TEMPLATEPATH . '/moduls/carausel/vip-ads.php');
+include (TEMPLATEPATH . '/moduls/index/top-organizations.php');
+include (TEMPLATEPATH . '/moduls/carausel/partners.php');
+include (TEMPLATEPATH . '/moduls/index/vibor-nas-100.php');
+include (TEMPLATEPATH . '/moduls/index/ads-gorod.php');
+get_footer(); // подключаем footer.php
+?>
