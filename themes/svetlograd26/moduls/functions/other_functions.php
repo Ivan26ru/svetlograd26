c31b680be085519b04f8557e3dfb2256c4fe8ef3
Ivan26ru <it-1ivan@yandex.ru>
2018-09-20 09:37:11 +0300

    улучшил функцию вывода телефонного номера, теперь работает хорошо

--- themes/svetlograd26/moduls/functions/other_functions.php
+++ themes/svetlograd26/moduls/functions/other_functions.php
@@ -1,20 +1,24 @@
 <?php
 
 
-// функция превращает любое числов в строку формата XX-XX-... или XXX-XX-XX-... в зависимости от четности кол-ва цифр
+// функция превращает любое числов в строку формата XX-XX-X или X-XXX-XXX-XX-XX в читабельный и цифровой формат
 function phone_read($phone,$type=''){
+// $phone номер телефона
+// $type тип вывода:
+	// number: +71231234545
+	// text: 8(123) 123-45-45
 	if (strlen($phone)==5) {
 		// echo "из 5 символов";
 		$patterns_text = '/(\d{1})(\d{2})(\d{2})/';
 		$replace_text = '$1-$2-$3';
-		$phone_text = '+786547'.$phone;
+		$phone_text = '8(86547) ' . preg_replace ($patterns_text, $replace_text, $phone);
 
-		$phone_number = preg_replace ($patterns_number, $replace_number, $phone);
+		$phone_number = '+786547'.$phone;
 
 	}elseif (strlen($phone)<=12) {
 		// echo "из 11 или 12 символов";
 		$patterns_text = '/(8|\+7){1}(\d{3})(\d{3})(\d{2})(\d{2})/';
-		$replace_text = '8($2)$3-$4-$5';
+		$replace_text = '8($2) $3-$4-$5';
 		$phone_text = preg_replace ($patterns_text, $replace_text, $phone);
 
 		$patterns_number = '/(^8)/';
@@ -25,10 +29,12 @@ function phone_read($phone,$type=''){
 
 if ($type==='number') {
 		$number=$phone_number;
-	}elseif ($type==='text') {
+
+}elseif ($type==='text') {
 		$number=$phone_text;
 };
-	return $number;
+
+return $number;
 
 };
 
