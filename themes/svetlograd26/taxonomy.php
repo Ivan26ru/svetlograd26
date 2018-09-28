1ae5a53de49d07191da71b7933190b99051b5437
Ivan26ru <it-1ivan@yandex.ru>
2018-09-28 08:30:50 +0300

    подбираю параметры для цикла таксономии с метками

--- themes/svetlograd26/taxonomy.php
+++ themes/svetlograd26/taxonomy.php
@@ -1,7 +1,28 @@
 <?php
 get_header(); // подключаем header.php
 
-include (TEMPLATEPATH . '/moduls/tax/category_organizacii.php');
+// include (TEMPLATEPATH . '/moduls/tax/category_organizacii.php');
+var_dump($term);
+$term = get_queried_object(); // данные элемента текущей таксономии
+$args = array(
+  // 'post_type' => 'post_organizacii',
+  // 'category_organizacii'=>$term->slug
+);
+
+$query = new WP_Query( $args );
+
+while( $query->have_posts() ) {
+  $query->next_post();
+
+the_title();
+}
+pagination();
+
+// Восстанавливаем оригинальные данные поста
+wp_reset_postdata();
+
+
+
 // include (TEMPLATEPATH . '/moduls/index/top-organizations.php');
 // include (TEMPLATEPATH . '/moduls/index/ads-gorod.php');
 get_footer(); // подключаем footer.php
