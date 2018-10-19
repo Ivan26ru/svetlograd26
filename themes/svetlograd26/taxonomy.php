4eb62232d00c8b9aaba1ca58cd76b0cddf2aba87
Ivan26ru <it-1ivan@yandex.ru>
2018-10-19 08:53:21 +0300

    тестирую вывод улиц на отдельной странице

--- themes/svetlograd26/taxonomy.php
+++ themes/svetlograd26/taxonomy.php
@@ -1,25 +1,58 @@
 <?php
 get_header(); // подключаем header.php
-
+// global $wp_query;
+// print_r($wp_query);
 // include (TEMPLATEPATH . '/moduls/tax/category_organizacii.php');
-var_dump($term);
 $term = get_queried_object(); // данные элемента текущей таксономии
+// var_dump($term);
+//       object(WP_Term)#11714 (10) {
+//   ["term_id"]=>
+//   int(38)
+//   ["name"]=>
+//   string(4) "mira"
+//   ["slug"]=>
+//   string(4) "mira"
+//   ["term_group"]=>
+//   int(0)
+//   ["term_taxonomy_id"]=>
+//   int(38)
+//   ["taxonomy"]=>
+//   string(5) "ulica"
+//   ["description"]=>
+//   string(0) ""
+//   ["parent"]=>
+//   int(0)
+//   ["count"]=>
+//   int(3)
+//   ["filter"]=>
+//   string(3) "raw"
+// }
+
+          $term = get_queried_object(); // данные элемента текущей таксономии
 $args = array(
   // 'post_type' => 'post_organizacii',
   // 'category_organizacii'=>$term->slug
+	'taxonomy'=>'ulica',
+	'numberposts' => 3,
 );
 
-$query = new WP_Query( $args );
-
+$query = new WP_Query($args);
+var_dump($query);
 while( $query->have_posts() ) {
   $query->next_post();
 
+// include (TEMPLATEPATH . '/moduls/tax/loop_tax.php');
 the_title();
 }
 pagination();
 
 // Восстанавливаем оригинальные данные поста
 wp_reset_postdata();
+
+// pagination();
+
+// wp_reset_postdata();
+// Восстанавливаем оригинальные данные поста
 
 
 
